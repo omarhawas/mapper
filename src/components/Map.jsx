e90b8b436b3a176e9120ea6091a651c917c69f9f
Omar Hawas <omarhawas94@gmail.com>
2023-03-08 13:52:10 -0500

    first commit. map is working

---
+++ src/components/Map.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+const Map = () => {
+  const defaultProps = {
+    center: {
+      lat: 56.1304,
+      lng: 106.3468,
+    },
+    zoom: 5,
+  };
+
+  return (
+    // Important! Always set the container height explicitly
+
+    <div style={{ height: "80vh", width: "75%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
+      </GoogleMapReact>
+    </div>
+  );
+};
+
+export default Map;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className="testing">Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
